14ca7b579ab33da05ab363a26642aa48fdc09f77
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-19 01:45:22 +0530

    project modal created

--- src/pages/Home/Projects.tsx
+++ src/pages/Home/Projects.tsx
@@ -1,54 +1,12 @@
 import { NorthEast } from '@mui/icons-material'
 import { Box, Fab, Grid2, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import { projects } from '../../ProjectsList'
+import { useState } from 'react'
+import ProjectModal from '../ProjectModal'
 
 function Projects() {
-  const navigate = useNavigate()
-
-  const projects = [
-    {
-      id: 1,
-      image: 'Images/Projects/Artigala.png',
-      title: 'Artigala Ayurveda',
-      type: 'Web & Mobile Design',
-      to: '/projects/artigala'
-    },
-    {
-      id: 2,
-      image: 'Images/Projects/Redwood.png',
-      title: 'Redwood Cabins',
-      type: 'Web Design',
-      to: '/projects/redwood'
-    },
-    {
-      id: 3,
-      image: 'Images/Projects/Cinepro.png',
-      title: 'Cinepro',
-      type: 'Web Design',
-      to: '/projects/cinepro'
-    },
-    {
-      id: 4,
-      image: 'Images/Projects/Blackpool.png',
-      title: 'Blackpool Redesign',
-      type: 'Web Design',
-      to: '/projects/blackpool'
-    },
-    {
-      id: 5,
-      image: 'Images/Projects/CodeQuest.png',
-      title: 'CodeQuest',
-      type: 'Web Design',
-      to: '/projects/code-quest'
-    },
-    {
-      id: 6,
-      image: 'Images/Projects/Prime.png',
-      title: 'Prime Ceylon',
-      type: 'Web Design',
-      to: '/projects/prime-ceylon'
-    }
-  ]
+  const [showProjectModal, setShowProjectModal] = useState<boolean>(false)
+  const [selectedProjectId, setSelectedProjectId] = useState<number>(0)
 
   return (
     <div id='portfolio'>
@@ -67,8 +25,8 @@ function Projects() {
           {projects.map((project, index) => (
             <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={index} sx={{ textAlign: 'start' }}>
               <img
-                src={project.image} alt={project.title} className='img-fluid mb-4 w-100'
-                style={{ borderRadius: '8px' }}
+                src={project.image} alt={project.title} className='mb-4 w-100'
+                style={{ borderRadius: '8px', objectFit: 'cover' }} width={'350px'} height={'200px'}
               />
               <div className='d-flex justify-content-between align-items-end'>
                 <div>
@@ -76,8 +34,8 @@ function Projects() {
                   <Typography variant='body1'>{project.type}</Typography>
                 </div>
                 <Fab
-                  size='small' color='secondary' href={project.to} sx={{ boxShadow: 'none' }}
-                  onClick={() => navigate('/project', { state: { projectId: project.id } })}
+                  size='small' color='secondary' sx={{ boxShadow: 'none' }}
+                  onClick={() => { setShowProjectModal(true); setSelectedProjectId(project.id) }}
                 >
                   <NorthEast sx={{ color: 'common.white' }} />
                 </Fab>
@@ -86,6 +44,7 @@ function Projects() {
           ))}
         </Grid2>
       </div>
+      <ProjectModal projectId={selectedProjectId} open={showProjectModal} closeModal={() => setShowProjectModal(false)} />
     </div>
   )
 }
