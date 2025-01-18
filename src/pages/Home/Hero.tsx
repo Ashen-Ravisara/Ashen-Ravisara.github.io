4632a878bbf642751b784f756c7c95246da96a7c
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-19 03:22:50 +0530

    mobile hero responsiveness

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -5,14 +5,11 @@ function Hero() {
   const theme = useTheme();
 
   return (
-    <Box
-      id='home' pt={10}
-      sx={{ position: 'relative', height: '100dvh' }}
-    >
+    <Box id='home' sx={{ pt: 10, height: '100dvh', overflow: 'hidden' }} >
       {useMediaQuery(theme.breakpoints.up('sm')) ? (
         <img src='/Images/bg_desktop.png' height={'100%'} width='100%' style={{ objectFit: 'contain' }} />
       ) : (
-        <img src='/Images/bg_phone.png' width='100%' style={{ objectFit: 'cover' }} />
+        <img src='/Images/bg_phone.png' style={{ objectFit: 'cover', width: '100%'}} />
       )}
     </Box>
   );
