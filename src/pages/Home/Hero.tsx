706882c25ca7967ab4db72bb169b78188b63c700
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-19 03:04:44 +0530

    hero converted to image

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,5 +1,5 @@
 
-import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 
 function Hero() {
   const theme = useTheme();
@@ -7,26 +7,13 @@ function Hero() {
   return (
     <Box
       id='home' pt={10}
-      sx={{
-        position: 'relative', height: '100dvh',
-        backgroundImage: "url('/Images/HeroBg.png')",
-        backgroundRepeat: 'no-repeat',
-        backgroundPositionY: 'bottom',
-        backgroundPositionX: 'center'
-      }}
+      sx={{ position: 'relative', height: '100dvh' }}
     >
-      <div className="container">
-        <img src="/Images/HeroButton.png" width={120} className="img-fluid" />
-        <Typography variant={useMediaQuery(theme.breakpoints.up('sm')) ? 'h1' : 'h3'} fontFamily='Urbanist' textAlign='center'>
-          I'm <span style={{ color: theme.palette.secondary.main }}>Ashen Ravisara</span>
-        </Typography>
-        <Typography variant={useMediaQuery(theme.breakpoints.up('sm')) ? 'h1' : 'h3'} fontFamily='Urbanist' textAlign='center' fontWeight='bold'>
-          UI / UX Designer
-        </Typography>
-      </div>
-      <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}>
-        <img src="/Images/Ashen.png" width={460} className='img-fluid' />
-      </div>
+      {useMediaQuery(theme.breakpoints.up('sm')) ? (
+        <img src='/Images/bg_desktop.png' height={'100%'} width='100%' style={{ objectFit: 'contain' }} />
+      ) : (
+        <img src='/Images/bg_phone.png' width='100%' style={{ objectFit: 'cover' }} />
+      )}
     </Box>
   );
 }
