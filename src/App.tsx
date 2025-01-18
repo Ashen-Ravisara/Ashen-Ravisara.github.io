2b0e7fcea0ff9dd043cb70213aeba2388c9d4722
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-18 20:12:03 +0530

    app initialised

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './Styles/App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App
